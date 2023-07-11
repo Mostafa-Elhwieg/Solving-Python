124a1c5abdb9e8aaedf075b43d3946923dc5f78a
DESKTOP-GSECK01 <mostafa.elhwieg@gmail.com>
2023-07-11 19:08:47 +0300

    Assignment 1 Completed

---
+++ Elzero Problems/Assignment #1.py
@@ -0,0 +1,22 @@
+# -----------------Assignment #1-----------------
+# This is Assignment Number 1
+# It's gonna be a long journey
+# -----------------Assignment #2-----------------
+myName = "Mostafa Adel"
+myAge = "20"
+myCountry = "Egypt"
+print("-"*30)
+# -----------------Assignment #3-----------------
+print("\"Name: " + myName + "\"")
+print("\"Age: " + myAge + "\"")
+print("\"Country: " + myCountry + "\"")
+print("-"*30)
+# -----------------Assignment #4-----------------
+print("\"Hello, My Name is "+myName+" And Iam "+myAge+" Years Old and I Live in "+myCountry+"\"")
+print("-"*30)
+# -----------------Assignment #5-----------------
+print(type(myName))
+print(type(myAge))
+print(type(myCountry))
+print("-"*30)
+
