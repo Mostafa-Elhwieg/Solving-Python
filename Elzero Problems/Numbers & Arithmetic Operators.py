66d9c7ed95768822c0086ab644015086cf0042c7
DESKTOP-GSECK01 <mostafa.elhwieg@gmail.com>
2023-07-13 19:51:15 +0300

    Numbers & Arithmetic Operators is completed

---
+++ Elzero Problems/Numbers & Arithmetic Operators.py
@@ -0,0 +1,36 @@
+# -----------------Assignment #1-----------------
+print(type(5))
+print(type(-3))
+print(type(3.52))
+print(type(-6.42))
+print(type(3+5j))
+
+print("-" * 50)
+
+# -----------------Assignment #2-----------------
+complexNumber = 1+2j
+print(f"The Imaginary Part Is: {complexNumber.imag}")
+print(f"The Real Part Is: {complexNumber.real}")
+
+print("-" * 50)
+
+# -----------------Assignment #3-----------------
+num = 10
+print(f"{num:.10f}")
+
+print("-" * 50)
+# -----------------Assignment #4-----------------
+num = 159.650
+print(int(num))
+print(type(int(num)))
+
+print("-" * 50)
+
+# -----------------Assignment #5-----------------
+print(100 - 115)
+print(50 * 30)
+print(21 % 4)
+print(110 // 11)
+print(97 // 20)
+
+print("-" * 50)
